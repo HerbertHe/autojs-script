7c60f156f2fc11e4fd3745ea4b103f8306a20e00
HerbertHe <Herbert.He0229@gmail.com>
2021-02-13 23:57:13 +0800

    feat :sparkles: : 初始化新的脚本

---
+++ fucksb天天报.js
@@ -0,0 +1,24 @@
+home();
+app.launchApp("企业微信");
+// sleep(1000)
+
+// 生成体温
+function generateTemp() {
+
+}
+
+// 填空家庭住址
+
+// 忽略填空联系人信息
+
+function fuckSBTianTianBao() {
+    setScreenMetrics(device.width, device.height)
+    click(600, 2300)
+    // sleep(500)
+    click(200, 2000)
+    sleep(2000)
+    click(540, 900)
+    sleep(500)
+}
+
+fuckSBTianTianBao()
